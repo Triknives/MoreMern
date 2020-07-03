bded4a7c8dc839bb9b1a0331340d42280d757260
Triknives <cordell.desu@gmail.com>
2020-07-03 12:38:11 -0700

    begin fcreating user components

--- src/user/components/pages/Users.js
+++ src/user/components/pages/Users.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import UsersList from '../components/UsersList';
 
 const Users = () => {
-    return <h1>Users Works</h1>
-}
+    const USERS = [{
+        id: 'u1',
+        name: 'Brett Cordell',
+        image:'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4d/Cat_November_2010-1a.jpg/1200px-Cat_November_2010-1a.jpg'}
+    ];
+
+
+    return <UsersList items={USERS}/>;
+};
 
 export default Users;
